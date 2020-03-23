75158fb11fa465f537fd3ce54133b377f83efbcd
skroys <53998111+skroys@users.noreply.github.com>
2020-03-23 22:52:34 +0530

    allowedhost

--- cms/cms/settings.py
+++ cms/cms/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o%+1w=x=x1xpmh!=0n&p%94(8fqim79dcy%k$kmm0rwpgv%30r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
